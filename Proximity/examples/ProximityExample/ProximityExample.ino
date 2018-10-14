b349308d0352c0dfb5cd7d8013fc804c5e7e1321
kendell <kendell.r@outlook.com>
2018-10-14 08:04:54 -0700

    Update ProximityExample.ino

--- Proximity/examples/ProximityExample/ProximityExample.ino
+++ Proximity/examples/ProximityExample/ProximityExample.ino
@@ -17,18 +17,26 @@ void setup() {
 }
 int IR;
 void loop() {
+  //Check if amount of IR light is greater than threshold
   if (prox.close()) {
+    //Say the threshold and that it recieved more IR light than the threshold
     Serial.print("Close to object, threshold is ");
     Serial.println(String(prox.getThreshold()));
   } else {
+    //Say the threshold and that it recieved less IR light than the threshold
     Serial.print("Not close to object, threshold is ");
     Serial.println(String(prox.getThreshold()));
   }
+  //If user sent data
   if (Serial.available()) {
+    //Get number that user sent
     int num = Serial.parseInt();
+    //Set new threshold
     prox.setThreshold(num);
+    //Say the new threshold
     Serial.print("Changed threshold to ");
     Serial.println(num);
   }
+  //Delay
   delay(250);
 }
