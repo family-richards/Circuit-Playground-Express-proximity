127354e4d7bf900cd433629aebd33bd512bf0f9c
kendell <kendell.r@outlook.com>
2019-02-20 07:44:25 -0500

    Update NeoPixelProximityWithClass.ino

--- NeoPixelProximityWithClass.ino
+++ NeoPixelProximityWithClass.ino
@@ -2,7 +2,7 @@
 #include <Proximity.h>
 
 #if !defined(ARDUINO_SAMD_CIRCUITPLAYGROUND_EXPRESS)
-#error "Infrared support is only for the Circuit Playground Express, it doesn't work with the Classic version"
+#error "Infrared support is only for the Circuit Playground Express, it doesn't work with other boards."
 #endif
 Proximity prox;
 void setup() {
